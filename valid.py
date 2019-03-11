82a2b66fa1d4c8b7265841bca244e6794130316c
Honorisverum <cheekibreeki322@gmail.com>
2019-03-12 01:59:56 +0300

    ref

--- valid.py
+++ valid.py
@@ -7,10 +7,19 @@
 
 import utils
 import torch
+import numpy as np
 
 
 REINITIALIZE_GAP = 4
-ACCURACY_CALC_GAP = 10
+ACCURACY_CALC_GAP = 9
+INFO_STRING = "Title: {video_title} | " \
+              "Mean Reward: {mean_reward} | " \
+              "Number of frames: {n_frames} | " \
+              "Number of fails: {n_fails} |"
+OVERALL_INFO_STRING = "Total Mean overlap: " \
+                      "{overlap} |" \
+                      "Total Robustness: " \
+                      "{robustness}"
 
 
 def compute_tests(testing_set_videos, net, pad_len, use_gpu):
@@ -19,6 +28,8 @@ def compute_tests(testing_set_videos, net, pad_len, use_gpu):
         torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
     net.eval()
+
+    all_overlaps, all_fails, all_frames = [], 0, 0
 
     for video in testing_set_videos:
 
@@ -56,7 +67,7 @@ def compute_tests(testing_set_videos, net, pad_len, use_gpu):
 
             # check if overlap is 0, else add to the answer
             if overlap == 0:
-                video.test_fails += 1
+                video.n_fails += 1
                 reinitialize_wait = True
                 wait_frame = REINITIALIZE_GAP
                 accuracy_wait_frames = ACCURACY_CALC_GAP
@@ -64,7 +75,35 @@ def compute_tests(testing_set_videos, net, pad_len, use_gpu):
                 if accuracy_wait_frames != 0:
                     accuracy_wait_frames -= 1
                     continue
-                video.test_predictions.append(overlap)
-                video.non_fail_frames += 1
+                video.predictions.append(overlap)
 
+        all_fails += video.n_fails
+        all_frames += video.len
+        all_overlaps += video.predictions
+        ep_video_overlap = np.mean(video.predictions).item() if video.predictions != [] else 0
+
+        # print info
+        info_format = {
+            'video_title': video.title,
+            'mean_reward': round(ep_video_overlap, 6),
+            'n_frames': video.len,
+            'n_fails': video.n_fails
+        }; print(INFO_STRING.format(**info_format))
+
+        # clear for next episode
+        video.n_fails = 0
+        video.predictions = []
+
+    ep_overlap = np.mean(all_overlaps).item() if all_overlaps != [] else 0
+    ep_robustness = 100.0 * all_fails / all_frames
+
+    # print overall info
+    overall_info_format = {
+        'overlap': round(ep_overlap, 6),
+        'robustness': round(ep_robustness, 6)
+    }; print(OVERALL_INFO_STRING.format(**overall_info_format))
+
+    print("***********************************************\n\n")
+
+    return net
 
